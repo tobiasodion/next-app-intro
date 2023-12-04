bc64c920a107c1c651e9f2e59a92cb7e3f5a4ecb
tobias odion <tobiasodion@gmail.com>
2023-12-04 02:47:49 +0100

    Remove middleware logic

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 
 export const middleware = (request) => {
-    return NextResponse.redirect(new URL('/', request.url))
+    //return NextResponse.redirect(new URL('/', request.url))
 }
 
 export default middleware
